d010107a257864579036488cfc7a718759f08596
Nicolas Karasiak <nicart@gmail.com>
2019-11-05 21:17:29 +0100

    add side-by-side for map and update to new geotiff library

--- maps/treespeciesformosat2/map_leaflet.js
+++ maps/treespeciesformosat2/map_leaflet.js
@@ -1,66 +1,85 @@
 var listOfSpecies = ['Silver birch','Oak','Red oak','Douglas fir','Eucalyptus','European Ash','Aspen','Corsican pine','Maritime pine','Black pine','Black locust','Silver fir','Willow']
 var colorOfSpecies = ["#d7d79e", "#267300", "#ff0000","#00a9e6","#d3ffbe","#4ce600","#e6e600","#a900e6","#e600a9","#005ce6","#784c0b","#00c5ff","#d1ff73"]
 var newOrder = [0,1,2,6,5,10,12,4,7,8,9,11,3];
-let map = L.map('map',{tap: !L.Browser.mobile}).setView([43.45,1.2],12);
-var marker;
+var colorMap = values => values[0] === 0 ? null :
+									(values[0] > 0 && values[0] <= 1) ? colorOfSpecies[0]:
+									(values[0] > 1 && values[0] <= 2) ? colorOfSpecies[1]:
+									(values[0] > 2 && values[0] <= 3) ? colorOfSpecies[2]:
+									(values[0] > 3 && values[0] <= 4) ? colorOfSpecies[3]:
+									(values[0] > 4 && values[0] <= 5) ? colorOfSpecies[4]:
+									(values[0] > 5 && values[0] <= 6) ? colorOfSpecies[5]:
+									(values[0] > 6 && values[0] <= 7) ? colorOfSpecies[6]:
+									(values[0] > 7 && values[0] <= 8) ? colorOfSpecies[7]:
+									(values[0] > 7 && values[0] <= 9) ? colorOfSpecies[8]:
+									(values[0] > 7 && values[0] <= 10) ? colorOfSpecies[9]:
+									(values[0] > 7 && values[0] <= 11) ? colorOfSpecies[10]:
+									(values[0] > 7 && values[0] <= 12) ? colorOfSpecies[11]:
+									(values[0] > 7 && values[0] <= 13) ? colorOfSpecies[12]:
+									colorOfSpecies[13];
+let map = L.map('map',{tap: !L.Browser.mobile}).setView([43.45,1.08],12);
 
-/* Dark basemap */
 let url = 'https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png';
+
 var OSM = L.tileLayer(url, {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="https://carto.com/attributions">CARTO</a>',
     subdomains: 'abcd'
 }).addTo(map);
-/**
 
-**/
+var GeoportailFrance_orthos = L.tileLayer('https://wxs.ign.fr/{apikey}/geoportail/wmts?REQUEST=GetTile&SERVICE=WMTS&VERSION=1.0.0&STYLE={style}&TILEMATRIXSET=PM&FORMAT={format}&LAYER=ORTHOIMAGERY.ORTHOPHOTOS&TILEMATRIX={z}&TILEROW={y}&TILECOL={x}', {
+	attribution: '<a target="_blank" href="https://www.geoportail.gouv.fr/">Geoportail France</a>',
+	bounds: [[-75, -180], [81, 180]],
+	minZoom: 2,
+	maxZoom: 19,
+	apikey: 'choisirgeoportail',
+	format: 'image/jpeg',
+	style: 'normal',
+  opacity : 1,
+}).addTo(map);
 
-plotty.addColorScale("speciesMap", colorOfSpecies, [0,1/12,2/12,3/12,4/12,5/12,6/12,7/12,8/12,9/12,10/12,11/12,1]);
+var url_to_geotiff_file = "modal_species_4326.tif";
 
-var speciesMap = L.leafletGeotiff(
-        url='modal_species_4326.tif',
-        options={band: 0,
-            displayMin: 1,
-            displayMax: 13,
-            name: 'modal classification',
-            colorScale: "speciesMap",
-            clampLow: false,
-            clampHigh: false,
-            //vector:true,
-        }
-    ).addTo(map);
+var speciesMap = fetch(url_to_geotiff_file)
+  .then(response => response.arrayBuffer())
+  .then(arrayBuffer => {
+    parseGeoraster(arrayBuffer).then(georaster => {
 
+      var layer = new GeoRasterLayer({
+          georaster: georaster,
+          pixelValuesToColorFn: colorMap,
+          resolution:256
+      });
 
-map.on('click', function(e) {
-var ROI = speciesMap.getValueAtLatLng(e.latlng.lat,e.latlng.lng);
+      layer.addTo(map)
 
-console.log(ROI)
-if(ROI > 0)
-{  console.log(listOfSpecies[ROI-1])
-  var popup = L.popup()
-    .setLatLng(e.latlng)
-    .setContent(`Species : ${listOfSpecies[ROI-1]}`)
-    .openOn(map);
-}
+      //map.fitBounds(layer.getBounds());
+
+      map.on('click', function(evt) {
+      var latlng = map.mouseEventToLatLng(evt.originalEvent);
+      ROI = geoblaze.identify(georaster, [latlng.lng, latlng.lat]);
+
+      if(ROI > 0)
+            {
+                var popup = L.popup()
+                .setLatLng(evt.latlng)
+                .setContent(`Species : ${listOfSpecies[ROI-1]}`)
+                .openOn(map);
+						}
+      });
+  	sideBySide = L.control.sideBySide(GeoportailFrance_orthos,layer);
+		sideBySide.addTo(map);
+  });
 });
 
-
 var legend = L.control({position: 'bottomright'});
-
-legend.onAdd = function (map) {
-
+legend.onAdd = function (speciesMap) {
     var div = L.DomUtil.create('div', 'info legend');
     // loop through our density intervals and generate a label with a colored square for each interval
     for (var i = 0; i < newOrder.length; i++) {
         div.innerHTML +=
             '<i style="background-color:' + colorOfSpecies[newOrder[i]] + '"></i> ' + listOfSpecies[newOrder[i]] + '<br>';
     }
-
     return div;
 };
 
 legend.addTo(map);
 L.control.scale().addTo(map);
-
-/** map.scrollWheelZoom.disable(); **/
-
-/** L.control.sideBySide(osmLayer,stamenLayer).addTo(map); **/
